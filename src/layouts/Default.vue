0381c80f88eba32db904accb8b6e6ba40ba726a5
Giuliano Varriale <giuliano.oca@gmail.com>
2018-11-15 18:48:10 -0200

    Add talwind and create new home page

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,42 +1,76 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link :to="{ name: 'home' }">Gridsome</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" :to="{ name: 'home' }">Home</g-link>
-        <g-link class="nav__link" :to="{ name: 'about' }">About</g-link>
-      </nav>
-    </header>
     <slot/>
   </div>
 </template>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
 
-.layout {
-  max-width: 600px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
+  /**
+  * This injects Tailwind's base styles, which is a combination of
+  * Normalize.css and some additional base styles.
+  *
+  * You can see the styles here:
+  * https://github.com/tailwindcss/tailwindcss/blob/master/css/preflight.css
+  *
+  * If using `postcss-import`, use this import instead:
+  *
+  * @import "tailwindcss/preflight";
+  */
+  @tailwind preflight;
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
+  body {
+    font-family: Montserrat, sans-serif;
+  }
 
-.nav__link {
-  margin-left: 20px;
-}
+  /**
+  * This injects any component classes registered by plugins.
+  *
+  * If using `postcss-import`, use this import instead:
+  *
+  * @import "tailwindcss/components";
+  */
+  @tailwind components;
+
+  /**
+  * Here you would add any of your custom component classes; stuff that you'd
+  * want loaded *before* the utilities so that the utilities could still
+  * override them.
+  *
+  * Example:
+  *
+  * .btn { ... }
+  * .form-input { ... }
+  *
+  * Or if using a preprocessor or `postcss-import`:
+  *
+  * @import "components/buttons";
+  * @import "components/forms";
+  */
+
+  /**
+  * This injects all of Tailwind's utility classes, generated based on your
+  * config file.
+  *
+  * If using `postcss-import`, use this import instead:
+  *
+  * @import "tailwindcss/utilities";
+  */
+  @tailwind utilities;
+
+  /**
+  * Here you would add any custom utilities you need that don't come out of the
+  * box with Tailwind.
+  *
+  * Example :
+  *
+  * .bg-pattern-graph-paper { ... }
+  * .skew-45 { ... }
+  *
+  * Or if using a preprocessor or `postcss-import`:
+  *
+  * @import "utilities/background-patterns";
+  * @import "utilities/skew-transforms";
+  */
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,86 @@
 <template>
-  <Layout>
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    <h1>Hello, world!</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
+  <Layout class="container mx-auto mt-10 lg:mt-20 px-4 lg:px-32">
+    <div class="flex items-center">
+      <img
+        src="@/assets/profile.jpg"
+        alt="Foto de Giuliano Varriale"
+        class="w-32 md:w-auto"
+      >
+
+      <h1 class="font-bold text-lg md:text-2xl lg:text-3xl pl-4 lg:pl-10">
+        Hi, I’m Giuliano Varriale, 30, front-end developer
+      </h1>
+    </div>
+
+    <ul class="list-reset my-10 lg:my-20 mx-10 lg:mx-20">
+      <li class="mb-4">
+        <a href="github.com/giulianovarriale" target="_blank" class="font-bold">
+          github.com/giulianovarriale
+        </a>
+      </li>
+
+      <li class="mb-4">
+        <a href="linkedin.com/in/giuliano-varriale-33983571/" class="font-bold">
+          linkedin.com/in/giuliano-varriale-33983571/
+        </a>
+      </li>
+
+      <li class="mb-4">
+        <a href="mailto:giuliano.oca@gmail.com" class="font-bold">
+          giuliano.oca@gmail.com
+        </a>
+      </li>
+    </ul>
+
+    <div class="text-grey-darker leading-loose index-about">
+      <p class="mb-5">
+        I started to work with the internet as a web designer, designing and developing websites for
+        digital agencies. During this time, I have learned the frontend development foundations like
+        HTML and CSS and a little bit of javascript - mostly, jQuery plugins initializations.
+
+        The main challenges in this time were related to the layouts and its creative navigations and
+        interactions, since agencies love creating innovative user experiences.
+      </p>
+
+      <p class="mb-5">
+        As time went by, the web became more dynamic and interactive. In order to build a better user
+        experience, things like route handling, views rendering and business logic became frontend
+        responsibilities, all of it, provided by javascript and its hundreds frameworks and libraries.
+        Besides that, nodejs came out, bringing with it, an entire new ecosystem with amazing tools.
+      </p>
+
+      <p class="mb-5">
+        I really wanted to work with this new whole environment, so I decided to look for a new job
+        where I could use it in my daily basis. I started to work for a software boutique company
+        called Codeminer where I have learned software design concepts, best practices and, of course,
+        javascript. There, I also had my first experience working in a large code base, which turned
+        completely my thoughts about how to build an web application.
+      </p>
+
+      <p class="mb-5">
+        Since then, I’ve been learning everyday, developing and improving features for known companies
+        such as Leroy Merlin, Enjoei and Consulta Remédios, which is the company I currently work for
+        remotely.
+      </p>
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+  @media (min-width: 768px) {
+    .index-about {
+      column-count: 2;
+      column-gap: 60px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .index-about {
+      column-gap: 90px;
+    }
+  }
+</style>
